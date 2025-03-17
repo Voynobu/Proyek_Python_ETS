2417df4d9a195814932c80584cc6b77a1005b1a3
nauvalryan08 <nauvalryan08@gmail.com>
2025-03-17 12:04:40 +0700

    Update Jadwal.py

--- Models/Jadwal.py
+++ Models/Jadwal.py
@@ -1,8 +1,10 @@
-from datetime import datetime, time
-
 #Author : Zaidan Zulkaisi Setiaji
 #NIM/Kelas : 241524031/1A-D4
 #Deskripsi : class untuk Jadwal
+
+import json
+from datetime import datetime, time
+from Utils.FileHandler import load_data, save_data
 
 class Jadwal:
     def __init__(self, dokter, hari, jam_awal, jam_akhir):
@@ -11,20 +13,18 @@ class Jadwal:
         self.jam_awal = jam_awal
         self.jam_akhir = jam_akhir
 
-    def StatusDokter(self):
-        saatini = datetime.now()
-        Hari_saatini = saatini.strftime("%A")
-        Waktu_saatini = saatini.time()
+    def status_dokter(self):
+        saat_ini = datetime.now()
+        hari_saat_ini = saat_ini.strftime("%A")
+        waktu_saat_ini = saat_ini.time()
 
         jam_awal = datetime.strptime(self.jam_awal, "%H:%M").time()
         jam_akhir = datetime.strptime(self.jam_akhir, "%H:%M").time()
 
-        if Hari_saatini == self.hari and jam_awal <= Waktu_saatini <= jam_akhir:
-            return "Available"
-        else:
-            return "Unavailable"
+        return "Available" if hari_saat_ini == self.hari and jam_awal <= waktu_saat_ini <= jam_akhir else "Unavailable"
 
     def __str__(self):
-        status = self.StatusDokter()
+        status = self.status_dokter()
         return f"{self.dokter} -- {self.hari} ({self.jam_awal} - {self.jam_akhir}). Status : {status}"
+
     
