0b1c9fbdc825071ef963f2cb83b5bd3592ef7245
Rangga <rangga.m.fajar99@gmail.com>
2025-03-17 19:51:21 +0700

    Update MainWindow.py

--- Window/MainWindow.py
+++ Window/MainWindow.py
@@ -1,4 +1,8 @@
-# main.py
+# MainWindow.py
+#Nama: Rangga Muhamad Fajar
+#Kelas: 1A - D4
+#NIM: 241524026
+
 from PyQt5 import QtWidgets, QtCore
 import sys
 from WindowSplashScreen import WindowSplashScreen
@@ -6,18 +10,20 @@ from WindowSplashScreen import WindowSplashScreen
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+        # Set judul dan ukuran window utama
         self.setWindowTitle("Main Window")
         self.setGeometry(100, 100, 1600, 900)
         self.initUI()
     
     def initUI(self):
+        # Menampilkan label selamat datang di tengah window
         label = QtWidgets.QLabel("Selamat Datang di Aplikasi!", self)
         label.setAlignment(QtCore.Qt.AlignCenter)
         self.setCentralWidget(label)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    # Mulai dengan Splash Screen
+    # Mulai dengan menampilkan Splash Screen
     splash = WindowSplashScreen()
     splash.show()
     sys.exit(app.exec_())
