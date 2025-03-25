21c4380647e41f38d360fc07358eea16a3c0ad74
Dino <mdidermwan@gmail.com>
2025-03-25 15:36:11 +0700

    Update WindowLoginAdmin.py

--- Window/WindowLoginAdmin.py
+++ Window/WindowLoginAdmin.py
@@ -6,22 +6,29 @@
 #Desc: - Tampilan login untuk admin.
 #      - Memeriksa email dan password dan jika berhasil membuka MainWindow.
 
+#Nama : Muhamad Dino Dermawan
+#Nim : 241524015
+#desc :  -menghubungkan ke WindowMenuAdmin 
+
 from PyQt5 import QtCore, QtGui, QtWidgets
+import json
+import os
+from WindowMenuAdmin import Ui_WindowMenuAdmin  # Nama file dan kelas diperbarui
 
 class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(1600, 900)
-        Dialog.setWindowFlags(QtCore.Qt.FramelessWindowHint)
-        
-        # Set background untuk login admin
-        self.label = QtWidgets.QLabel(Dialog)
+    def setupUi(self, windowLoginAdmin):  # Perubahan nama dari windowAdminLogin
+        windowLoginAdmin.setObjectName("windowLoginAdmin")
+        windowLoginAdmin.resize(1600, 900)
+        windowLoginAdmin.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+
+        # Set background
+        self.label = QtWidgets.QLabel(windowLoginAdmin)
         self.label.setGeometry(QtCore.QRect(-4, 0, 1611, 901))
-        self.label.setPixmap(QtGui.QPixmap("C:/Users/Rangga/Documents/KULIAH/SEMESTER 2/PROYEK 1/TUBES PRA ETS/ASSETS/BACKGROUND/2.png"))
+        self.label.setPixmap(QtGui.QPixmap("C:/Users/muhamad dino/Desktop/SEMESTER 2/PENGEMBANGAN PERANGKAT LUNAK DESKTOP/TubesUTS/ASSETS/ASSETS/BACKGROUND/2.png"))
         self.label.setScaledContents(True)
-        
-        # Field untuk Email
-        self.lineEdit = QtWidgets.QLineEdit(Dialog)
+
+        # Username field
+        self.lineEdit = QtWidgets.QLineEdit(windowLoginAdmin)
         self.lineEdit.setGeometry(QtCore.QRect(200, 390, 521, 61))
         self.lineEdit.setStyleSheet("""
             QLineEdit {
@@ -34,10 +41,10 @@ class Ui_Dialog(object):
                 border-bottom: 4px solid #ffbd59;
             }
         """)
-        self.lineEdit.setPlaceholderText("Masukkan Username Anda!")
-        
-        # Field untuk Password
-        self.lineEdit_2 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit.setPlaceholderText("Masukkan Username!")
+
+        # Password field
+        self.lineEdit_2 = QtWidgets.QLineEdit(windowLoginAdmin)
         self.lineEdit_2.setGeometry(QtCore.QRect(200, 520, 521, 61))
         self.lineEdit_2.setStyleSheet("""
             QLineEdit {
@@ -52,59 +59,90 @@ class Ui_Dialog(object):
         """)
         self.lineEdit_2.setPlaceholderText("Masukkan Password Anda!")
         self.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
-        
-        self.show_password_button = QtWidgets.QPushButton(Dialog)
+
+        # Show password button
+        self.show_password_button = QtWidgets.QPushButton(windowLoginAdmin)
         self.show_password_button.setGeometry(QtCore.QRect(676, 520, 61, 61))
         self.show_password_button.setStyleSheet("border: none;")
-        self.show_password_button.setIcon(QtGui.QIcon("C:/Users/Rangga/Documents/KULIAH/SEMESTER 2/PROYEK 1/TUBES PRA ETS/BUTTON/BUTTON/EYE.png"))
+        self.show_password_button.setIcon(QtGui.QIcon("C:/Users/muhamad dino/Desktop/SEMESTER 2/PENGEMBANGAN PERANGKAT LUNAK DESKTOP/TubesUTS/ASSETS/ASSETS/BUTTON/EYE.png"))
         self.show_password_button.setIconSize(QtCore.QSize(40, 40))
         self.show_password_button.setCheckable(True)
         self.show_password_button.clicked.connect(self.toggle_password)
-        
+
         # Tombol Login
-        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton = QtWidgets.QPushButton(windowLoginAdmin)
         self.pushButton.setGeometry(QtCore.QRect(330, 660, 251, 91))
-        self.pushButton.setStyleSheet("border-image: url(C:/Users/Rangga/Documents/KULIAH/SEMESTER 2/PROYEK 1/TUBES PRA ETS/BUTTON/BUTTON/LOGIN.png);")
+        self.pushButton.setStyleSheet("border-image: url(C:/Users/muhamad dino/Desktop/SEMESTER 2/PENGEMBANGAN PERANGKAT LUNAK DESKTOP/TubesUTS/ASSETS/ASSETS/BUTTON/LOGIN.png);")
         self.add_hover_effect(self.pushButton)
-        
+        self.pushButton.clicked.connect(self.login)
+
         # Tombol Back
-        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2 = QtWidgets.QPushButton(windowLoginAdmin)
         self.pushButton_2.setGeometry(QtCore.QRect(10, 20, 111, 101))
-        self.pushButton_2.setStyleSheet("border-image: url(C:/Users/Rangga/Documents/KULIAH/SEMESTER 2/PROYEK 1/TUBES PRA ETS/BUTTON/BUTTON/BACK.png);")
+        self.pushButton_2.setStyleSheet("border-image: url(C:/Users/muhamad dino/Desktop/SEMESTER 2/PENGEMBANGAN PERANGKAT LUNAK DESKTOP/TubesUTS/ASSETS/ASSETS/BUTTON/BACK.png);")
         self.add_hover_effect(self.pushButton_2)
-        
+        self.pushButton_2.clicked.connect(windowLoginAdmin.close)
+
+        # Raise semua elemen agar layer GUI benar
         self.label.raise_()
         self.pushButton.raise_()
         self.lineEdit.raise_()
         self.lineEdit_2.raise_()
         self.show_password_button.raise_()
         self.pushButton_2.raise_()
-        
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
-    
-    def retranslateUi(self, Dialog):
+
+        self.retranslateUi(windowLoginAdmin)
+        QtCore.QMetaObject.connectSlotsByName(windowLoginAdmin)
+
+    def add_hover_effect(self, button):
+        effect = QtWidgets.QGraphicsOpacityEffect()
+        button.setGraphicsEffect(effect)
+        button.enterEvent = lambda event: effect.setOpacity(0.7)
+        button.leaveEvent = lambda event: effect.setOpacity(1.0)
+
+    def retranslateUi(self, windowLoginAdmin):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Login Admin"))
-    
+        windowLoginAdmin.setWindowTitle(_translate("windowLoginAdmin", "Login Admin"))
+
     def toggle_password(self):
         if self.show_password_button.isChecked():
             self.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Normal)
         else:
             self.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
-    
-    def add_hover_effect(self, button):
-        # Tambahkan efek hover pada tombol
-        button.enterEvent = lambda event: self.set_button_opacity(button, 0.7)
-        button.leaveEvent = lambda event: self.set_button_opacity(button, 1.0)
 
-    def set_button_opacity(self, button, opacity):
-        # Terapkan efek opacity pada tombol
-        effect = QtWidgets.QGraphicsOpacityEffect(button)
-        effect.setOpacity(opacity)
-        button.setGraphicsEffect(effect)
+    def login(self):
+        username = self.lineEdit.text()
+        password = self.lineEdit_2.text()
 
-# Kelas pembungkus untuk WindowLoginAdmin
+        if self.cek_login(username, password):
+            self.open_window_login_admin()
+        else:
+            QtWidgets.QMessageBox.warning(None, "Login Gagal", "Username atau password salah.")
+
+    def cek_login(self, username, password):
+        if not os.path.exists("admin_data.json"):
+            data = {"admins": [{"username": "AdminRumahSakit", "password": "RumahAdminSakit"}]}
+            with open("admin_data.json", "w") as file:
+                json.dump(data, file)
+
+        with open("admin_data.json", "r") as file:
+            data = json.load(file)
+
+        for admin in data["admins"]:
+            if admin["username"] == username and admin["password"] == password:
+                return True
+        return False
+
+    def open_window_login_admin(self):
+        try:
+            self.windowLoginAdmin = QtWidgets.QMainWindow()
+            self.ui = Ui_WindowMenuAdmin()  # Nama kelas diperbarui
+            self.ui.setupUi(self.windowLoginAdmin)
+            self.windowLoginAdmin.show()
+            self.window().close()
+        except Exception as e:
+            print(f"Error: {e}")
+
 class WindowLoginAdmin(QtWidgets.QDialog):
     def __init__(self):
         super().__init__()
@@ -119,11 +157,13 @@ class WindowLoginAdmin(QtWidgets.QDialog):
         self.welcome.show()
         self.close()
 
+# Bagian ini untuk testing langsung GUI
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
+    window = QtWidgets.QMainWindow()
     ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    ui.setupUi(window)
+    window.show()
     sys.exit(app.exec_())
+
